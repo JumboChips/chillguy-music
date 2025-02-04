928873e5fa19d910ca86c3769bd9b456194d24b3
jumbochips <jumbochips1@gmail.com>
2025-02-04 17:14:02 +0900

    first commit

---
+++ chillguy_be/src/main/java/com/jumbochips/chillguy/ChillGuyApplication.java
@@ -0,0 +1,13 @@
+package com.jumbochips.chillguy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChillGuyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChillGuyApplication.class, args);
+    }
+
+}
---
+++ chillguy_be/src/test/java/com/jumbochips/chillguy/ChillGuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jumbochips.chillguy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChillGuyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
