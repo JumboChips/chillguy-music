ef325713bddbbb67cdb6c24427eb8ef59007be1d
jumbochips <jumbochips1@gmail.com>
2025-03-21 15:10:15 +0900

    refactor: Application class에 log.debug 테스트용 출력 코드 추가

--- chillguy_be/src/main/java/com/jumbochips/chillguy/ChillGuyApplication.java
+++ chillguy_be/src/main/java/com/jumbochips/chillguy/ChillGuyApplication.java
@@ -1,14 +1,17 @@
 package com.jumbochips.chillguy;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@Slf4j
 public class ChillGuyApplication {
 
     public static void main(String[] args) {
+        log.debug("DEBUG 로그 테스트");
         SpringApplication.run(ChillGuyApplication.class, args);
     }
 
